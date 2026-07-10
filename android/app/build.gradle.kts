732c132ec536df410e011660a3a5af7ea0b44048
Sujaykharat <sujaykharat2675@gmail.com>
2026-07-10 11:28:15 +0530

    chore: update package name to com.lakshyaaerotech.app across all platforms

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.lakshya_aerotech"
+    namespace = "com.lakshyaaerotech.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.lakshya_aerotech"
+        applicationId = "com.lakshyaaerotech.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
